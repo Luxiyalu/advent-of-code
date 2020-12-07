8000d2da3279f90851af105079292db324c83662
Lucia <doitelu@gmail.com>
2020-12-06 22:20:45 -0800

    Update first part to use linear instead of recursion; add typings and comments; clean up

--- day_7/solution.py
+++ day_7/solution.py
@@ -1,5 +1,6 @@
 import os
 import re
+from typing import List, Tuple
 
 dir = os.path.dirname(os.path.realpath(__file__))
 input = open(f"{dir}/input.txt", "r")
@@ -8,13 +9,14 @@ rules = input.read().splitlines()
 ### --- PARSING INPUT
 
 
-def parse_rules(rules):
-    top_down_dict = {}
-    down_top_dict = {}
+def parse_rules(rules: List[str]) -> Tuple[dict, dict]:
+    top_down_dict = {}  # container -> contained map
+    down_top_dict = {}  # contained -> container map
 
     for rule in rules:
         if "no other bags" in rule:
             continue
+
         container_color, contained_str = rule.split(" bags contain ")
         contained_colors = re.sub(r"\sbag(s)*\.*", "", contained_str).split(", ")
 
@@ -24,10 +26,9 @@ def parse_rules(rules):
             contained_color = f"{des} {color}"
 
             if contained_color not in down_top_dict:
-                down_top_dict[contained_color] = {container_color}
-            else:
-                down_top_dict[contained_color].add(container_color)
+                down_top_dict[contained_color] = set()
 
+            down_top_dict[contained_color].add(container_color)
             top_down_dict[container_color][f"{des} {color}"] = int(count)
 
     return top_down_dict, down_top_dict
@@ -39,37 +40,38 @@ top_down_dict, down_top_dict = parse_rules(rules)
 ### --- PART I
 
 
-def get_possible_containers(contained_color, possible_containers):
-    if contained_color in down_top_dict:
-        container_colors = down_top_dict[contained_color]
-        possible_containers.update(container_colors)
+def get_possible_containers(contained_color: str) -> int:
+    containers_to_count = [contained_color]
 
-        for container_color in container_colors:
-            colors = get_possible_containers(container_color, possible_containers)
-            possible_containers.update(colors)
+    for color in containers_to_count:
+        if color not in down_top_dict:
+            continue
 
-    return possible_containers
+        for container_color in down_top_dict[color]:
+            if container_color not in containers_to_count:
+                containers_to_count.append(container_color)
+
+    return len(containers_to_count) - 1
 
 
-possible_containers = get_possible_containers("shiny gold", set())
-print(len(possible_containers))
+print(get_possible_containers("shiny gold"))
 
 
 ### --- PART II
 
 
-def count_contained_bags(init_container_color):
-    total_contained_count = -1
-    bags_to_count = [(1, init_container_color)]
+def count_contained_bags(init_container_color: str) -> int:
+    total_bags_count = -1
+    bags_to_count = [(init_container_color, 1)]
 
-    for count, container_color in bags_to_count:
-        total_contained_count += count
+    for container_color, bag_count in bags_to_count:
+        total_bags_count += bag_count
 
         if container_color in top_down_dict:
-            for color, contained_count in top_down_dict[container_color].items():
-                bags_to_count.append((count * contained_count, color))
+            for color, count in top_down_dict[container_color].items():
+                bags_to_count.append((color, bag_count * count))
 
-    return total_contained_count
+    return total_bags_count
 
 
 print(count_contained_bags("shiny gold"))
